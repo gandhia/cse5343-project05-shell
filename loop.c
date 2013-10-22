de1dee7eb270867c8bfa69876575f57159e359d7
Avani Gandhi <agandhi@smu.edu>
2013-10-21 22:36:17 -0500

    Adding the uncompiled code for loop and test.

---
+++ loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+  {
+    printf("This is a test program to see if a simple loop runs\n");
+    int i;
+    for (i=0; i<10; i++)
+    {
+    	printf("%d  ", i);
+    }
+    printf("\n");
+    return 0;
+  }
+
---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+  {
+    printf("This is a test program to see if the executable works.\nIt looks like it works.\n");
+    return 0;
+  }
+
